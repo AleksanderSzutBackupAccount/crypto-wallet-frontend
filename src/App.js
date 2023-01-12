16a3e2bfc5340ef8530e279b01182da3724e4e81
Aleksander Szut <aleksander@croney.io>
2023-01-12 06:19:09 +0100

    speed up docker

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
     localStorage.getItem("user_crypto_currency_data") &&
     JSON.parse(localStorage.getItem("user_crypto_currency_data"));
 
+  const logged =
+    localStorage.getItem("loggedData") &&
+    JSON.parse(localStorage.getItem("loggedData"));
+
+  console.log(logged);
+
   const routing = useRoutes(getRoutes(user_crypto_currency_data));
 
   return <Layout>{routing}</Layout>;
